6787464e55ff8fdae182ad707764b8c70af3a2de
megan <meggsnbacon777@gmail.com>
2020-07-10 08:38:33 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -130,5 +130,5 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-programmer_hash[:dennis_ritchie][:languages][1].toinclude("Assembly")
+programmer_hash[:dennis_ritchie][:languages][1].to include("Assembly")
 end
